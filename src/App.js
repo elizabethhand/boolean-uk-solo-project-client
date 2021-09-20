4f3e39fff826e89a84127c437f316a5dac7af95b
Lizzie Hand <elizabethghand@gmail.com>
2021-09-20 12:19:00 +0100

    Style header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Switch, Route, Redirect } from "react-router-dom";
 import './App.css';
 import Homepage from "./pages/Homepage"
+import Header from "./pages/Header"
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <main>
         <Switch>
           <Route path="/" exact>
---
+++ src/pages/Header.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="header-title">
+                <h1> Meal-a-deal</h1>
+            </div>
+            <div className="header-links">
+                <p className="header-link"> Home</p>
+                <p className="header-link"> Login</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
