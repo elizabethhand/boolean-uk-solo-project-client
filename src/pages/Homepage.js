bda99df75d57597b77111c3585e4a30ff0797e16
Lizzie Hand <elizabethghand@gmail.com>
2021-09-20 10:29:19 +0100

    Style homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,22 +2,31 @@ import React from "react"
 
 function Homepage() {
     return (
-        <main>
-            <div> Map will go here</div>
-            <div className="categories">
-                <div className="category-tile"> Breakfast</div>
-                <div className="category-tile"> Juice</div>
-                <div className="category-tile"> American</div>
-                <div className="category-tile"> Coffee</div>
+        <main className="homepage">
+            <div className="homepage-container">
+                <div className="map-container">
+                    <div className="map"> Map will go here</div>
+                </div>
+                <div className="categories">
+                    <h2 className="category-title">Categories</h2>
+                    <div className="category-tiles">
+                        <div className="category-tile"> Breakfast</div>
+                        <div className="category-tile"> Juice</div>
+                        <div className="category-tile"> American</div>
+                        <div className="category-tile"> Coffee</div>
+                    </div>
+                </div>
+                <div className="best-deals">
+                    <h2 className="best-deals-title"> Best Deals</h2>
+                    <div className="deal-tiles">
+                        <div className="deal-tile"> Deal</div>
+                        <div className="deal-tile"> Deal</div>
+                        <div className="deal-tile"> Deal</div>
+                        <div className="deal-tile"> Deal</div>
+                    </div>
+                </div>
             </div>
-            <div className="best-deals">
-                <h1> Best Deals</h1>
-                <div className="deal-tile"> Deal</div>
-                <div className="deal-tile"> Deal</div>
-                <div className="deal-tile"> Deal</div>
-                <div className="deal-tile"> Deal</div>
-            </div>
-        </main>
+        </main >
     )
 }
 
